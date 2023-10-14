6cc8e38e853e5c43ecb8d8b09d7dc737e2db8223
linyisonger <752957127@qq.com>
2023-10-14 19:07:31 +0800

    .

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ import { Maths } from 'https://cdn.jsdelivr.net/npm/@3r/tool@1.3.2/index.js';
  * Draw a capsule
  */
 export function drawCapsule(x1, y1, x2, y2, radius) {
+    // 永远保持正方向
+    const dr = y1 == y2 && x1 > x2 ? -1 : 1;
     const p1 = v2(x1, y1);
     const p2 = v2(x2, y2);
-    const p3 = v2((p2.x + p1.x) / 2, (p2.y + p1.y) / 2);
-    const p4 = v2(p2.x - p1.x, p2.y - p1.y);
+    const p3 = v2(p2.x - p1.x, p2.y - p1.y);
     const xAxis = v2(1, 0);
-    const c1 = Vector2.getPointAtDist(p3, p2, radius);
-    const c2 = Vector2.getPointAtDist(p3, p2, -radius);
-    const ang = Vector2.includedAngle(p4, xAxis) * -1 - 90;
+    const ang = Vector2.includedAngle(p3, xAxis) * -1 + 90 * dr;
     const rad = Maths.degreeToRad(ang);
     this.beginPath();
-    this.arc(c1.x, c1.y, 20, 0 + rad, Math.PI + rad);
-    this.arc(c2.x, c2.y, 20, Math.PI + rad, rad);
+    this.arc(x1, y1, radius, 0 + rad, Math.PI + rad);
+    this.arc(x2, y2, radius, Math.PI + rad, rad);
     this.closePath();
     this.stroke();
     this.fill();
